342e294917ee7d5dd5c17e6935ca9a6d422aebb1
Miloš Sikimić <sikimic10@gmail.com>
2021-07-27 18:17:15 +0200

    first commit

---
+++ src/main/java/com/osa/ProjekatOsa2021/ProjekatOsa2021Application.java
@@ -0,0 +1,13 @@
+package com.osa.ProjekatOsa2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatOsa2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekatOsa2021Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/osa/ProjekatOsa2021/ProjekatOsa2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osa.ProjekatOsa2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjekatOsa2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
